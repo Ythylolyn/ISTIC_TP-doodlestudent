1a531179f3cd8184bbd6cf3595c2efd780e19c40
Ythylolyn <165142481+Ythylolyn@users.noreply.github.com>
2024-03-27 21:03:51 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#build API et FRONT :
+cd api && docker build . -t doodlestudent-api
+cd ..
+cd front && docker build . -t doodlestudent-front
+cd ..
 
+#run les docker_compose :
 cd api && docker-compose up -d
 cd ../front && docker-compose up -d
